b477ce237462a7fb8e024952e49af5be30dcf905
Shaw Young <shawson@gmail.com>
2020-04-22 20:39:56 +0100

    Creating bottom nav bar and stub pages

--- codebase/Dossier/.eslintrc.js
+++ codebase/Dossier/.eslintrc.js
@@ -2,5 +2,21 @@ module.exports = {
   root: true,
   extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        alias: {
+          _assets: './src/assets',
+          _components: './src/components',
+          _navigations: './src/navigations',
+          _pages: './src/pages',
+          _services: './src/services',
+          _styles: './src/styles',
+          _utils: './src/utils',
+        },
+      },
+    },
+  },
 };
