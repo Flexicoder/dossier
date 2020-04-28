266ef248aabb2b12f9ec3d327979377853f3f0bb
Paul Ledger <1398301+Flexicoder@users.noreply.github.com>
2020-04-28 14:37:49 +0100

    Initial Key Information component, #21

--- codebase/Dossier/src/pages/KeyInfo.tsx
+++ codebase/Dossier/src/pages/KeyInfo.tsx
@@ -1,13 +1,29 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, SafeAreaView, StatusBar, View, ScrollView, Dimensions  } from 'react-native';
 import HospitalNumber from '../components/HospitalNumber'
+import KeyInformation from '../components/KeyInformation';
+import  styles  from '../assets/styles'
 
 const KeyInfoPage = () => {
+
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <SafeAreaView style={styles.scrollContainer}>
+      <StatusBar barStyle="light-content" backgroundColor="#468189" />
+      <ScrollView
+        style={{ flex: 1 }}
+        contentContainerStyle={styles.scrollview}
+        scrollEnabled={true}
+      >
+        <View style={styles.scrollContent}>
         <Text>Key Info</Text>
+        <KeyInformation name=""  />
         <HospitalNumber  name="Brighton" number="93993" />
-      </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+
+
+
     );
   };
 
